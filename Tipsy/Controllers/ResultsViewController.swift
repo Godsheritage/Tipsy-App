4edd35f78e3319ece6fc4ddb1c1be35141441645
Godsheritage  Adeoye <Adeoyegodsheritage@gmail.com>
2023-02-06 16:30:18 -0500

    First_changes

---
+++ Tipsy/Controllers/ResultsViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ResultsViewController.swift
+//  Tipsy
+//
+//  Created by Godsheritage Adeoye on 2/6/23.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view
+    }
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var settingsLabel: UILabel!
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
